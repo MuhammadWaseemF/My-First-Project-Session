6252baada9e4de281b4809f4507ed85638835964
MuhammadWaseemF <160323697+MuhammadWaseemF@users.noreply.github.com>
2024-03-19 22:32:04 +0500

    i have solved question no 2
    question 2 solved out of 45

---
+++ p2.js
@@ -0,0 +1,5 @@
+// TESRA SAWAL HAMARA HE NAME CASES KA //////
+var personName = "muhammad waseem farooqi,";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
---
+++ p2.ts
@@ -0,0 +1,7 @@
+// TESRA SAWAL HAMARA HE NAME CASES KA //////
+let personName:string = "muhammad waseem farooqi,"
+console.log(personName.toLowerCase());
+
+console.log(personName.toUpperCase());
+
+console.log(personName.replace(/\b\w/g, (char) => char.toUpperCase()));
